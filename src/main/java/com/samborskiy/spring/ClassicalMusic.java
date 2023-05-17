052287c043d15536f75ba4db0634d07f7a225a54
zaharsamborskiy <117350706+zaharsamborskiy@users.noreply.github.com>
2023-05-18 00:01:42 +0700

    next5 commit

--- src/main/java/com/samborskiy/spring/ClassicalMusic.java
+++ src/main/java/com/samborskiy/spring/ClassicalMusic.java
@@ -1,8 +1,19 @@
 package com.samborskiy.spring;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {}
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
+    }
+    public void doMyInit() {
+        System.out.println("Initialization " + ClassicalMusic.class.getName());
+    }
+    public void doMyDestroy() {
+        System.out.println("Destruction " + ClassicalMusic.class.getName());
     }
 }
