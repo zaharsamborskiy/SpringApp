7ff05ae355fe6a01607271a23eadcc376d072120
zaharsamborskiy <117350706+zaharsamborskiy@users.noreply.github.com>
2023-05-17 23:21:52 +0700

    next3 commit

--- src/main/java/com/samborskiy/spring/MusicPlayer.java
+++ src/main/java/com/samborskiy/spring/MusicPlayer.java
@@ -1,13 +1,48 @@
 package com.samborskiy.spring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
 
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> music) {
+        this.musicList = music;
     }
+
+    public MusicPlayer() {
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(List<Music> music) {
+        this.musicList = music;
+    }
+
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
